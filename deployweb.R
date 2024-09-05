7d2e9848885db8c811fccc3e2ac4e20184f91ebe
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-09-05 17:21:40 -0300

    Update deployweb.R

--- deployweb.R
+++ deployweb.R
@@ -1,1 +1,16 @@
+install.packages('rsconnect')
+library (rsconnect)
 
+rsconnect::setAccountInfo(name= ${{ secrets.SHINYAPPS_NAME}},
+                          token=${{ secrets.SHINYAPPS_TOKEN}},
+                          secret=${{ secrets.SHINYAPPS_SECRET}})
+rsconnect::deployApp("~/Desktop/R_analysis/webapp",
+                     appFiles = c("app.R",
+                                  "data_update.R",
+                                  "funcao_processamento.R",
+                                  "about.md",
+                                  "umidade.R",
+                                  "variable.R",
+                                  "temperatura.R",
+                                  "co2.R")
+                     )
