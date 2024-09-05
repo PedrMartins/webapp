6c2544f2eab8a00361418e38fb2d3e15eb5560bd
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-09-05 16:59:18 -0300

    Update deployweb.R

--- deployweb.R
+++ deployweb.R
@@ -1,17 +1,1 @@
-install.packages('rsconnect')
-library (rsconnect)
-
-rsconnect::setAccountInfo(name='pedro-rufino-13021991',
-                          token='CA30B0338DD2F76AEED6C6909E5B78D7',
-                          secret='Ycqoz/R8hsOToeu28g0BJQumh6SlVjwHQ3chk5cG')
-rsconnect::deployApp("~/Desktop/R_analysis/webapp",
-                     appFiles = c("app.R",
-                                  "data_update.R",
-                                  "funcao_processamento.R",
-                                  "about.md"))
-
-
-
-all_c = colors()
-str_subset(all_c, "purple")
 
