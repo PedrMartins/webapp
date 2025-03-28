15f39eaf74ccec352cdbfb9c6ed046b8d154c0d1
Pedro <pedro.rufino.martins@gmail.com>
2025-03-28 16:18:05 -0300

    library script

--- library_package.R
+++ library_package.R
@@ -1,3 +1,11 @@
+pkg <- c("shiny","shinythemes","stringr","lubridate",
+         "dplyr","markdown","leaflet","sf","DT")
+
+pkg <- pkg[!pkg%in%installed.packages()]
+pkg
+install.packages (pkg)
+
+
 library(shiny)
 library(shinythemes)
 library(stringr)
